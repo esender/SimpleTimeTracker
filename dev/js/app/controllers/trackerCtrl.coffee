507d5ac6603bf4e2b731d7681c30efbffcacd912
Marat Abdulin <esend.work@gmail.com>
2014-04-04 16:18:20 +0400

    Start and Stop Timer

--- dev/js/app/controllers/trackerCtrl.coffee
+++ dev/js/app/controllers/trackerCtrl.coffee
@@ -3,29 +3,24 @@
     '$scope',
     '$timeout',
     ($scope, $timeout) ->
-      console.log('1')
-      $scope.timers = [
-        {
-          title: 'Start Work With Simple Time Tracker'
-          started_at: new Date
-          description: 'Simple Description'
-        }
-        {
-          title: 'Started Two'
-          started_at: new Date
-          description: ''
-        }
-      ]
+      timer = null
+      $scope.timers = []
 
-      timer = ->
+      nextTimer = ->
         $scope.seconds = Math.floor((new Date().getTime() - $scope.currentTimer.started_at.getTime()) / 1000)
-        $timeout timer, 1000
+        timer = $timeout nextTimer, 1000
 
       $scope.startNewTimer = () ->
         $scope.newTimer.started_at = new Date
         $scope.currentTimer = $scope.newTimer
         $scope.newTimer = {}
-        $timeout(timer, 1000);
+        timer = $timeout nextTimer, 1000
+
+      $scope.stopNewTimer = () ->
+        $scope.currentTimer.ended_at = new Date
+        $scope.timers.push($scope.currentTimer)
+        $scope.currentTimer = null
+        $timeout.cancel timer
 
 
 
--- dist/js/app/trackerCtrl.js
+++ dist/js/app/trackerCtrl.js
@@ -1,28 +1,24 @@
 (function() {
   this.TimeTracker.controller('TrackerCtrl', [
     '$scope', '$timeout', function($scope, $timeout) {
-      var timer;
-      console.log('1');
-      $scope.timers = [
-        {
-          title: 'Start Work With Simple Time Tracker',
-          started_at: new Date,
-          description: 'Simple Description'
-        }, {
-          title: 'Started Two',
-          started_at: new Date,
-          description: ''
-        }
-      ];
-      timer = function() {
+      var nextTimer, timer;
+      timer = null;
+      $scope.timers = [];
+      nextTimer = function() {
         $scope.seconds = Math.floor((new Date().getTime() - $scope.currentTimer.started_at.getTime()) / 1000);
-        return $timeout(timer, 1000);
+        return timer = $timeout(nextTimer, 1000);
       };
-      return $scope.startNewTimer = function() {
+      $scope.startNewTimer = function() {
         $scope.newTimer.started_at = new Date;
         $scope.currentTimer = $scope.newTimer;
         $scope.newTimer = {};
-        return $timeout(timer, 1000);
+        return timer = $timeout(nextTimer, 1000);
+      };
+      return $scope.stopNewTimer = function() {
+        $scope.currentTimer.ended_at = new Date;
+        $scope.timers.push($scope.currentTimer);
+        $scope.currentTimer = null;
+        return $timeout.cancel(timer);
       };
     }
   ]);
